6d5ea41f0fce19dccaddddbed587ac17a0c995fe
Ammar <1071535@hr.nl>
2024-10-15 14:05:02 +0200

    Restaurant

---
+++ PresentationLayer/Restaurant.cs
@@ -0,0 +1,11 @@
+class Restaurant
+{
+    public void Menu()
+    {
+        Console.WriteLine("1. Login");
+        Console.WriteLine("2. Sign Up");
+        Console.WriteLine("3. Enter as Guest");
+        Console.WriteLine("3. Admin Login");
+        Console.WriteLine("5. Exit");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ class Program
 {
     public static void Main()
     {
+
     }
 }
