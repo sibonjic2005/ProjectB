9b52946c67fa197229c45c7d19ee5c22932f73f0
Ewickie <cgericchen@outlook.com>
2024-10-22 14:30:48 +0200

    update presentation layer

--- obj/Debug/net8.0/ProjectB.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35578e996126f5e16f3f409ce945701a055ed37c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9735e92355a448f022466542fa48a93c4cdab56c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
